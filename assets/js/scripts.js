81a9f49f9caeb431400acac2c2197ff0ce22c604
z3r0stack <z3r0stack@gmail.com>
2017-07-25 21:32:24 +0700

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,53 +1,102 @@
 (function ($) {
-    // Dropdown menu
+// Dropdown menu
     $(function () {
         $('.js-navbar__toggle').on('click', function () {
             $('.js-navbar').toggleClass('is-opened');
             $('.js-navbar__toggle').attr('aria-expanded', $('.js-navbar').hasClass('is-opened'));
             return false;
         });
-
         $('.js-navbar a').each(function(i, link) {
             link = $(link);
-
-            link.on('click', function(e) {
+            link.on('touchend', function(e) {
                 if(
                     link.parent().hasClass('has-submenu') &&
-                    $('.js-navbar__toggle').attr('aria-expanded') === 'true' &&
+                    $('.js-navbar').hasClass('is-opened') &&
                     link.parent().attr('aria-expanded') !== 'true'
                 ) {
                     e.preventDefault();
+                    e.stopPropagation();
                     link.parent().attr('aria-expanded', 'true');
                 }
+                
+                $('.js-navbar li[aria-expanded="true"]').each(function(i, item) {                	
+                	if(!$.contains(item, link[0])) {
+                		$(item).attr('aria-expanded', 'false');
+                	}
+                });
             });
         });
     });
-
-    // iOS :hover fix
+	
+	 // iOS :hover fix
     document.addEventListener("touchend", function() {});
+	
+	// EU Cookie Law Bar
+	$(window).on("load", function () {
+		var cookieBar = $('.js-cookie-bar');
+		
+		if(!cookieBar.length) {
+			return;	
+		}
+		
+		var cookieBarClose = cookieBar.find('.cookie-bar__close');
+		
+		cookieBarClose.on('click', function(event) {
+			event.preventDefault();
+			localStorage.setItem('theme-cookie-bar', true);	
+			cookieBar.removeClass('is-sticky');
+		});
+		
+		if(!localStorage.getItem('theme-cookie-bar')) {
+			cookieBar.addClass('is-sticky');	
+		}
+	});	
+	
+	// Sticky menu animation
+	$(function($) {
+		var menu = $('.js-top');
+		
+		if(!menu.length) {
+			return;
+		}
+		
+		var previousScroll = $(window).scrollTop();
+		var menuHeight = menu.outerHeight();
+		var menuTop = 0;
+		var headerHeight = menu.outerHeight(true);
+		
+		$(window).on('scroll', function() {
+			var currentScroll = $(window).scrollTop();
+			var diff = currentScroll - previousScroll;
+			menuTop -= diff;
+			
+			if(menuTop < -menuHeight) {
+				menuTop = -menuHeight;
+			}
+			
+			if(menuTop >= 0) {
+				menuTop = 0;
+			}
+			
+			if(currentScroll <= headerHeight + 50) {
+				menu.removeClass('is-sticky');
+				menu.parent().css('padding-top', "0px");
+			} else {
+				menu.addClass('is-sticky');
+				menu.parent().css('padding-top', headerHeight + "px");
+			}
 
-    // EU Cookie Law Bar
-    $(window).on("load", function () {
-        var cookieBar = $('.js-cookie-bar');
-
-        if (!cookieBar.length) {
-            return;
-        }
-
-        var cookieBarClose = cookieBar.find('.cookie-bar__close');
-
-        cookieBarClose.on('click', function (event) {
-            event.preventDefault();
-            localStorage.setItem('theme-cookie-bar', true);
-            cookieBar.removeClass('is-sticky');
-        });
-
-        if (!localStorage.getItem('theme-cookie-bar')) {
-            cookieBar.addClass('is-sticky');
-        }
-    });
-
-    // Mainmenu improvements
+            if (currentScroll <= 30) {
+                menuTop = 0;
+            }
+			
+			menu.css('top', menuTop + 'px');
+			
+			previousScroll = currentScroll;
+		});
+	});
+	
+	 // Mainmenu improvements
     $(function ($) {
         var mainmenu = $('.navbar__menu');
         var level0 = mainmenu.children('li');
@@ -100,105 +149,41 @@
             }
         }
     });
+	
+	// Share buttons pop-up
+	$(function () {
+		// link selector and pop-up window size
+		var Config = {
+			Link: ".js-share",
+			Width: 500,
+			Height: 500
+		};
+		// add handler links
+		var slink = document.querySelectorAll(Config.Link);
+		for (var a = 0; a < slink.length; a++) {
+			slink[a].onclick = PopupHandler;
+		}
+		// create popup
+		function PopupHandler(e) {
+			e = (e ? e : window.event);
+			var t = (e.target ? e.target : e.srcElement);
+			// popup position
+			var px = Math.floor(((screen.availWidth || 1024) - Config.Width) / 2),
+				py = Math.floor(((screen.availHeight || 700) - Config.Height) / 2);
+			// open popup
+			var link_href = t.href ? t.href : t.parentNode.href;
+			var popup = window.open(link_href, "social",
+				"width=" + Config.Width + ",height=" + Config.Height +
+				",left=" + px + ",top=" + py +
+				",location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1");
+			if (popup) {
+				popup.focus();
+				if (e.preventDefault) e.preventDefault();
+				e.returnValue = false;
+			}
 
-    // Sticky menu animation
-    $(function ($) {
-        var menu = $('.js-top');
-
-        var previousScroll = $(window).scrollTop();
-        var menuHeight = menu.outerHeight();
-        var menuTop = 0;
-
-        $(window).on('scroll', function () {
-            var currentScroll = $(window).scrollTop();
-            var diff = currentScroll - previousScroll;
-            menuTop -= diff / 2;
-
-            if(currentScroll <= 0) {
-                menuTop = 0 - currentScroll;
-            } else {
-                if (menuTop < -menuHeight) {
-                    menuTop = -menuHeight;
-                }
-
-                if (menuTop >= 0) {
-                    menuTop = 0;
-                    menu.addClass('is-sticky-on');
-
-                }
-            }
-
-            if(currentScroll <= 0) {
-                menu.removeClass('is-sticky-off');
-                menu.removeClass('is-sticky-on');
-
-            } else {
-               menu.addClass('is-sticky-off');
-
-            }
-
-            previousScroll = currentScroll;
-        });
-		
-	    $(window).trigger('scroll');
-
-    });
-
-    // Share buttons pop-up
-    $(function () {
-        // link selector and pop-up window size
-        var Config = {
-            Link: ".js-share",
-            Width: 500,
-            Height: 500
-        };
-        // add handler links
-        var slink = document.querySelectorAll(Config.Link);
-        for (var a = 0; a < slink.length; a++) {
-            slink[a].onclick = PopupHandler;
-        }
-        // create popup
-        function PopupHandler(e) {
-            e = (e ? e : window.event);
-            var t = (e.target ? e.target : e.srcElement);
-            // popup position
-            var px = Math.floor(((screen.availWidth || 1024) - Config.Width) / 2),
-                py = Math.floor(((screen.availHeight || 700) - Config.Height) / 2);
-            // open popup
-            var link_href = t.href ? t.href : t.parentNode.href;
-            var popup = window.open(link_href, "social",
-                "width=" + Config.Width + ",height=" + Config.Height +
-                ",left=" + px + ",top=" + py +
-                ",location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1");
-            if (popup) {
-                popup.focus();
-                if (e.preventDefault) e.preventDefault();
-                e.returnValue = false;
-            }
-
-            return !!popup;
-        }
-    });
-
-    // Post filters
-    $(function () {
-        if(!$('.filter').length) {
-            return;
-        }
-        $('.filter').find('.filter__item').each(function(i, item) {
-            item = $(item);
-
-            item.on('click', function(e) {
-                e.preventDefault();
-                $('.filter').find('.filter__item').removeClass('filter__item--active');
-                item.addClass('filter__item--active');
-
-                $('.card').removeClass('card--hidden');
-
-                if(item.attr('href') !== '#') {
-                    $('.card').not('[data-tags*="' + item.text() + ' "]').addClass('card--hidden');
-                }
-            });
-        });
-    });
+			return !!popup;
+		}
+	});
+	
 })(jQuery);
